44d038a5a8d305998ea296b534771776be226626
“NiBienvenu” <nicibienvenu@gmail.com>
2024-06-26 11:55:58 +0200

    chat row

--- resources/views/livewire/chat/chat-main.blade.php
+++ resources/views/livewire/chat/chat-main.blade.php
@@ -1,13 +1,62 @@
 <div>
-    <button
-        class="btn btn-primary"
-    >
-        Notifications
-        <span
-            class="badge bg-primary"
-            >12</span
-        >
-    </button>
+    <div class="row">
+        <div class="col-4">
+            <div class="row">
+                <div class="col-md-9 col-lg-10 chat-window">
+                    <header class="px-3 py-2 bg-white border-bottom chat-header">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h5 class="font-weight-bold mb-0">Chats</h5>
+                            <button class="btn">
+                                <svg class="bi bi-three-dots-vertical" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+                                </svg>
+                            </button>
+                        </div>
 
+                    </header>
+                    <main class="p-2 chat-main">
+                        <ul class="list-unstyled">
+                            <li class="py-3 hover-bg-light rounded-2xl transition flex gap-4 cursor-pointer px-2">
+                                <a href="#"class="shrink-0">
+                                    <img  style="border-radius: 50%" src="https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=USER" alt="">
+                                </a>
+                                <div class="flex-grow-1">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <h6 class="font-weight-bold mb-0">Receiver's Name</h6>
+                                        <small class="text-muted">10 mins ago</small>
+                                    </div>
+                                    <div class="d-flex gap-2 align-items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-all" viewBox="0 0 16 16">
+                                            <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z"/>
+                                            <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z"/>
+                                        </svg>
+                                        <p class="mb-0 text-truncate">Last message body</p>
+                                        @if (true)
+                                        <span class="badge bg-primary text-white">1</span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="dropdown">
+                                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                                            <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                                        </svg>
+                                    </button>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <li><a class="dropdown-item" href="#">View Profile</a></li>
+                                        <li><a class="dropdown-item" href="#">Delete</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </main>
+                </div>
+            </div>
+        </div>
+       <div class="col-6">
+
+       </div>
+    </div>
+    .
 
 </div>
